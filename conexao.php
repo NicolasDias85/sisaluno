b02340967920f9ba8865164f0e5bab1b6e0cd985
João Nicolas Dias Santana <131170619+NicolasDias85@users.noreply.github.com>
2023-08-12 22:14:57 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php 
 //Criar as constantes com as credencias de acesso ao banco de dados
-define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '');
+define('HOST', '10.70.230.53:3306');
+define('USUARIO', 'sisaluno');
+define('SENHA', 'sisaluno2023');
 define('DBNAME', 'sisaluno');
 
 //Criar a conexão com banco de dados usando o PDO e a porta do banco de dados
